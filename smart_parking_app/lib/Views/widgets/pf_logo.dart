1c087887adc45bb899b779c5523aaa2f17440341
Shenara DE SILVA <DELL@DESKTOP-VFSVIGN>
2025-10-01 15:56:07 +0530

    Removed firebase configurations and used the logo svg in pf_logo.dart file

--- smart_parking_app/lib/Views/widgets/pf_logo.dart
+++ smart_parking_app/lib/Views/widgets/pf_logo.dart
@@ -1,36 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class PFLogo extends StatelessWidget {
   final double size;
-  final Color? backgroundColor;
-  final Color? textColor;
 
-  const PFLogo({
-    super.key,
-    this.size = 50,
-    this.backgroundColor,
-    this.textColor,
-  });
+  const PFLogo({super.key, this.size = 50});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SvgPicture.asset(
+      'assets/pf_logo.svg', // exact path to SVG
       width: size,
       height: size,
-      decoration: BoxDecoration(
-        color: backgroundColor ?? const Color(0xFF2E5AAC),
-        borderRadius: BorderRadius.circular(size / 2),
-      ),
-      child: Center(
-        child: Text(
-          'PF',
-          style: TextStyle(
-            color: textColor ?? Colors.white,
-            fontSize: size * 0.4,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
     );
   }
 }
--- smart_parking_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ smart_parking_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,7 @@
 import FlutterMacOS
 import Foundation
 
-import cloud_firestore
 import file_selector_macos
-import firebase_auth
-import firebase_core
 import flutter_local_notifications
 import flutter_secure_storage_macos
 import printing
@@ -16,10 +13,7 @@ import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
