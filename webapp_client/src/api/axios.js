06706dabd2253477ad117faf6bb80a984b4f6105
ShenaradeSilva <150214271+ShenaradeSilva@users.noreply.github.com>
2025-10-01 15:15:47 +0530

    Update axios.js

--- webapp_client/src/api/axios.js
+++ webapp_client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000", // FastAPI backend
+  baseURL: "http://localhost:5000", // FastAPI backend
   headers: {
     "Content-Type": "application/json",
   },
